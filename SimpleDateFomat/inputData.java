4e0a51918e908fd994475635410394b083195cb6
Nguyen Van Hieu <nvh@gmail.com>
2024-07-27 18:40:47 +0700

    Them bai SimpleDateFormat

---
+++ SimpleDateFomat/inputData.java
@@ -0,0 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.*;
+public class inputData {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        try{
+            System.out.println("Input Date: ");
+            Date date =df.parse(sc.nextLine());
+            System.out.println("Date: "+df.format(date));
+        }catch(Exception e){}
+    }
+}
